8a9d6e740d24f00c7588a874c0ae37f4bf1158a4
LucaSoftwareEngineer <lucapc40@gmail.com>
2024-09-11 18:18:49 +0200

    Add files via upload

---
+++ backend/hr360/src/main/java/com/luca_dev/Hr360Application.java
@@ -0,0 +1,13 @@
+package com.luca_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hr360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hr360Application.class, args);
+	}
+
+}
