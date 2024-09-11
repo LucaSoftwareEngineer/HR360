9f93d6b2056998683bfe84b86aab8287e7017247
LucaSoftwareEngineer <lucapc40@gmail.com>
2024-09-11 17:49:30 +0200

    creato modelli Amministratore e Dipendente

---
+++ src/main/java/com/luca_dev/models/Dipendente.java
@@ -0,0 +1,14 @@
+package com.luca_dev.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Dipendente {
+
+    
+
+}
---
+++ src/test/java/com/luca_dev/Hr360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luca_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hr360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
