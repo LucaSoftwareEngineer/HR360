f9f11191ff0eba8fe0569c762c4724059124944a
LucaSoftwareEngineer <lucapc40@gmail.com>
2024-09-11 17:52:37 +0200

    Inserito metodi get e set in dipendente

--- src/main/java/com/luca_dev/models/Dipendente.java
+++ src/main/java/com/luca_dev/models/Dipendente.java
@@ -9,6 +9,42 @@ import jakarta.persistence.Id;
 @Entity
 public class Dipendente {
 
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column
+    String nome;
+    String cognome;
+    String username;
+    String password;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getCognome() {
+        return this.cognome;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
 
 }
