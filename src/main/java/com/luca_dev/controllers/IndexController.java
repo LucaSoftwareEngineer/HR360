5fb66dc3fcbaf4da601240979cbe79aaecd61916
LucaSoftwareEngineer <lucapc40@gmail.com>
2024-09-15 10:03:21 +0200

    modificato nome del metodo root nel indexController in appStart

--- src/main/java/com/luca_dev/controllers/IndexController.java
+++ src/main/java/com/luca_dev/controllers/IndexController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
     
     @GetMapping("/")
-    public String getMethodName() {
+    public String appStart() {
         return "HR360 START OK";
     }
     
