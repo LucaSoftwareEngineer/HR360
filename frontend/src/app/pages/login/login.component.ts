d30bcecfe895d9f65b8524fba08bb4f3987e8357
LucaSoftwareEngineer <lucapc40@gmail.com>
2024-09-24 18:43:14 +0200

    implementato nel frontend funzionalità httpClient al metodo loginDipendente

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,11 +1,12 @@
 import { HttpClient, HttpClientModule, provideHttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -13,16 +14,28 @@ export class LoginComponent {
 
   httpClient = inject(HttpClient);
   public data: Array<any> = [];
+
+  username = new FormControl();
+  password = new FormControl();
+
   constructor(httpClient: HttpClient) {}
+
   ngOnInit() {
   }
 
   loginDipendente() {
-    this.httpClient.get('http://127.0.0.1:8080/dipendente/login/?username=luca&password=luca')
+    let username_value = this.username.value;
+    let password_value = this.password.value;
+    this.httpClient.get('http://127.0.0.1:8080/dipendente/login?username='.concat(username_value).concat('&password=').concat(password_value))
       .subscribe({
         next: (data: any) => {
           console.log(data);
           this.data = data;
+          if (data == 0) {
+            console.log("accesso negato");
+          } else {
+            console.log("accesso consentito");
+          }
         }, error: (err) => console.log(err)
       });
   }
